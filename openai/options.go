18ae56176e18c51c07cda0dc910679c3238c36b1
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-03-08 14:18:40 +0800

    chore(openai): add comments.

--- openai/options.go
+++ openai/options.go
@@ -1,42 +1,51 @@
 package openai
 
-// Option specifies instrumentation configuration options.
+// Option is an interface that specifies instrumentation configuration options.
 type Option interface {
 	apply(*config)
 }
 
-var _ Option = (*optionFunc)(nil)
-
+// optionFunc is a type of function that can be used to implement the Option interface.
+// It takes a pointer to a config struct and modifies it.
 type optionFunc func(*config)
 
+// Ensure that optionFunc satisfies the Option interface.
+var _ Option = (*optionFunc)(nil)
+
+// The apply method of optionFunc type is implemented here to modify the config struct based on the function passed.
 func (o optionFunc) apply(c *config) {
 	o(c)
 }
 
+// WithToken is a function that returns an Option, which sets the token field of the config struct.
 func WithToken(val string) Option {
 	return optionFunc(func(c *config) {
 		c.token = val
 	})
 }
 
+// WithOrgID is a function that returns an Option, which sets the orgID field of the config struct.
 func WithOrgID(val string) Option {
 	return optionFunc(func(c *config) {
 		c.orgID = val
 	})
 }
 
+// WithModel is a function that returns an Option, which sets the model field of the config struct.
 func WithModel(val string) Option {
 	return optionFunc(func(c *config) {
 		c.model = val
 	})
 }
 
+// WithProxyURL is a function that returns an Option, which sets the proxyURL field of the config struct.
 func WithProxyURL(val string) Option {
 	return optionFunc(func(c *config) {
 		c.proxyURL = val
 	})
 }
 
+// config is a struct that stores configuration options for the instrumentation.
 type config struct {
 	token    string
 	orgID    string
