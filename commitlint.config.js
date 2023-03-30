73b8598ac19d03bb387faae196ca2711a0392ce0
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-03-30 14:21:45 +0800

    chore: update

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always', 'Infinity'],
+    'footer-max-line-length': [0, 'always', 'Infinity'],
   },
 };
