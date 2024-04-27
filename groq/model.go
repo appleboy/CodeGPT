15c4919df52b7281110d442e1742f330ca82540e
Bo-Yi Wu <appleboy.tw@gmail.com>
2024-04-27 15:04:50 +0800

    feat(groq): update codebase to support new AI models (#155)
    - Update model references from `LLaMA2-70b-chat`, `Mixtral-8x7b-Instruct-v0.1`, and `Gemma-7b-it` to `llama3-8b-8192`, `llama3-70b-8192`, `mixtral-8x7b-32768`, and `gemma-7b-it` in README.md
    - Replace old model constants with new ones in `groq/model.go`
    - Remove deprecated `GetModel` function from `groq/model.go`
    - Update model validation cases to use new model constants in `groq/model.go`
    - Update model mappings in `openai/openai.go` to reflect new model constants
    - Adjust completion function in `openai/openai.go` to use new model strings
    - Add new models to the allowed function call list in `openai/openai.go`
    
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- groq/model.go
+++ groq/model.go
@@ -3,38 +3,21 @@ package groq
 type Model string
 
 const (
-	LLaMA270bChat          Model = "LLaMA2-70b-chat"
-	Mixtral8x7bInstructV01 Model = "Mixtral-8x7b-Instruct-v0.1"
-	Gemma7bIt              Model = "Gemma-7b-it"
+	LLaMA38b    Model = "llama3-8b-8192" //
+	LLaMA370b   Model = "llama3-70b-8192"
+	Mixtral8x7b Model = "mixtral-8x7b-32768"
+	Gemma7b     Model = "gemma-7b-it"
 )
 
 func (m Model) String() string {
 	return string(m)
 }
 
-func (m Model) GetModel() string {
-	return GetModel(m)
-}
-
 func (m Model) IsVaild() bool {
 	switch m {
-	case LLaMA270bChat, Mixtral8x7bInstructV01, Gemma7bIt:
+	case LLaMA38b, LLaMA370b, Mixtral8x7b, Gemma7b:
 		return true
 	default:
 		return false
 	}
-}
-
-var model = map[Model]string{
-	LLaMA270bChat:          "llama2-70b-4096",
-	Mixtral8x7bInstructV01: "mixtral-8x7b-32768",
-	Gemma7bIt:              "gemma-7b-it",
-}
-
-// GetModel returns the model name.
-func GetModel(modelName Model) string {
-	if _, ok := model[modelName]; !ok {
-		return model[LLaMA270bChat]
-	}
-	return model[modelName]
 }
